cfb4ea71a3ee5393f517939b29b2f4bc42b2d1af
henry22 <henry811010@gmail.com>
2019-11-10 18:50:59 +0800

    Preserve the account in cookie through session

--- app.js
+++ app.js
@@ -3,30 +3,68 @@ const app = express()
 const port = 3000
 const session = require('express-session')
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const checkAccount = require('./check_account')
+
+let isLogin = false
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 30 * 1000
+  }
 }))
 
-app.use((req, res, next) => {
-  if (!req.session.views) {
-    req.session.views = {}
+app.get('/', (req, res) => {
+  if (req.session.email && req.session.password) {
+    name = req.session.firstName
+    isLogin = true;
+
+    res.render('welcome', { name: name, logstatus: isLogin })
+  } else {
+    res.render('index')
   }
-
-  // count the views
-  req.session.views['/'] = (req.session.views['/'] || 0) + 1
-
-  next()
 })
 
-app.get('/', (req, res) => {
-  const views = req.session.views['/']
-  res.render('index', { views: views })
+app.get('/welcome', (req, res) => {
+  if(req.session.email && req.session.password){
+    name = req.session.firstName
+    isLogin = true;
+
+    res.render('welcome', { name: name, logstatus: isLogin })
+  } else {
+    res.redirect('/')
+  }
+})
+
+// logout
+app.get('/logout', (req, res) => {
+  req.session.destroy()
+  isLogin = false
+  res.redirect('/')
+})
+
+app.post('/', (req, res) => {
+  const account = req.body
+  const isValidated = checkAccount(account).length ? true : false
+  const matchAccount = checkAccount(account)[0]
+
+  if (isValidated) {
+    req.session.email = matchAccount.email
+    req.session.password = matchAccount.password
+    req.session.firstName = matchAccount.firstName
+    res.redirect('/welcome')
+  } else {
+    res.render('index', { isValidated: !isValidated })
+  }
+
 })
 
 app.listen(port, () => console.log(`The server is listening on http:localhost:${port}`))
---
+++ check_account.js
@@ -0,0 +1,35 @@
+function checkAccount(account) {
+  const users = [
+    {
+      firstName: 'Tony',
+      email: 'tony@stark.com',
+      password: 'iamironman'
+    },
+    {
+      firstName: 'Steve',
+      email: 'captain@hotmail.com',
+      password: 'icandothisallday'
+    },
+    {
+      firstName: 'Peter',
+      email: 'peter@parker.com',
+      password: 'enajyram'
+    },
+    {
+      firstName: 'Natasha',
+      email: 'natasha@gamil.com',
+      password: '*parol#@$!'
+    },
+    {
+      firstName: 'Nick',
+      email: 'nick@shield.com',
+      password: 'password'
+    }
+  ]
+
+  const matchedAccount = users.filter(user => (user.email === account.email && user.password === account.password))
+
+  return matchedAccount
+}
+
+module.exports = checkAccount
